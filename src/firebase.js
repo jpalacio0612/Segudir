be6e62cde80ce8d5d3ce9937dd400b68b67ab71a
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-07 19:02:08 -0500

    title and favicon added

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB-xRWhu7m8WdpZiZwvp6eUwFf_2-NoSQE",
+  authDomain: "segudir.firebaseapp.com",
+  databaseURL: "https://segudir.firebaseio.com",
+  projectId: "segudir",
+  storageBucket: "segudir.appspot.com",
+  messagingSenderId: "981913502676",
+  appId: "1:981913502676:web:084688593218333c2c0c41",
+  measurementId: "G-N0JCHSDBJV"
+})
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const storage = firebase.storage()
+
+export { db, auth, storage }
