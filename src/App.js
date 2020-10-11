b5ba88a0358f8d1c6d12ab147cc7e68ccb9ed74a
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-10 20:55:39 -0500

    router transition added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { SignUp } from "./components/SignUp/SignUp";
 import { SignIn } from "./components/SignIn/SignIn";
 import { Courses } from "./components/courses/Courses";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
+  let location = useLocation();
+
   return (
-    <Router>
+    <>
       <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/courses" component={Courses} />
-      </Switch>
-    </Router>
+      <TransitionGroup>
+        <CSSTransition timeout={500} classNames="fade" key={location.key}>
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/courses" component={Courses} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </>
   );
 }
 
